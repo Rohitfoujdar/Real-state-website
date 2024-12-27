958591d9a8efd44f3a2a249787f6c4935842820b
Rohitfoujdar <rohitfoujdar8696@gmail.com>
2024-12-27 12:37:54 +0530

    12-84-Badge

--- client/src/components/cards/AdCard.js
+++ client/src/components/cards/AdCard.js
@@ -1,17 +1,61 @@
-import React from 'react'
+import React from "react";
+import { IoBedOutline } from "react-icons/io5";
+import { TbBath } from "react-icons/tb";
+import { BiArea } from "react-icons/bi";
+import { Badge } from "antd";
 
-export default function AdCard({ad}) {
+export default function AdCard({ ad }) {
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(ad?.price);
   return (
     <div className="col-lg-4 p-4 gx-4 gy-4">
+      <Badge.Ribbon
+        text={`${ad?.type} for ${ad?.action}`}
+        color={ad?.action == "Sell" ? "blue" : "red"}
+      >
         <div className="card hoverable shadow">
-            <img src={ad?.photos?.[0].Location} alt={`${ad?.type}-${ad?.address}-${ad?.action}-${ad?.price}`} style= {{height:"250px",  objectFit:"cover"}} />
-            <div className="card-body">
-                <div className="d-flex justify-content-betweent">
-                    <h3>{ad?.price}</h3>
-                </div>
-                <p>Ad Features</p>
+          <img
+            src={
+              ad?.type === "House"
+                ? "https://m.economictimes.com/thumb/height-450,width-600,imgsize-22382,msid-111780228/which-mansion-tops-the-list-of-the-worlds-most-expensive-houses.jpg"
+                : "https://i1.au.reastatic.net/712x480,smart=85,r=33,g=40,b=46,quality=60,progressive/002c0070af054375c1bdb7cb446c8d2b8a7edf11edcb06c648a369c0d038df0c/image0.jpg"
+            }
+            alt={`${ad?.type}-${ad?.address}-${ad?.action}-${ad?.price}`}
+            style={{ height: "250px", objectFit: "cover" }}
+          />
+          <div className="card-body">
+            <div className="mb-3">
+              <h3>{formattedPrice}</h3>
+              <h4 className="card-text">{ad?.address}</h4>
             </div>
+            <p className="card-text d-flex justify-content-between">
+              {ad?.bedrooms ? (
+                <span>
+                  <IoBedOutline /> {ad?.bedrooms}
+                </span>
+              ) : (
+                ""
+              )}
+              {ad?.bathrooms ? (
+                <span>
+                  <TbBath /> {ad?.bathrooms}
+                </span>
+              ) : (
+                ""
+              )}
+              {ad?.landsize ? (
+                <span>
+                  <BiArea /> {ad?.landsize}
+                </span>
+              ) : (
+                ""
+              )}
+            </p>
+          </div>
         </div>
+      </Badge.Ribbon>
     </div>
-  )
+  );
 }
